0ca9122730c91abeb2a0c592870f5cd825c53830
Dariusz Dabrowski <dariusz.dabrowski@ceneo.pl>
2017-11-25 07:41:12 +0100

    Chromosomes Fix, added ReplaceGenes method, and implement Clone method

--- GeneticAlghorithm.Lib/Chromosomes/Chromosome.cs
+++ GeneticAlghorithm.Lib/Chromosomes/Chromosome.cs
@@ -8,14 +8,14 @@ namespace GeneticAlghorithm.Lib.Chromosomes
 {
     public class Chromosome<T> : IChromosome<T>
     {
-        private readonly Random _random = new Random();
         private readonly IProblem<T> _problem;
         public double Fitness => GetFitnes();
-        public IList<Gene<T>> Genes { get; private set; }
+        public Gene[] Genes { get; private set; }
+        
         public Chromosome(IProblem<T> problem)
         {
             _problem = problem;
-            Genes = new List<Gene<T>>(problem.ChromosomeLength);
+            Genes = new Gene[problem.ChromosomeLength];
         }
 
         private double GetFitnes()
@@ -25,24 +25,50 @@ namespace GeneticAlghorithm.Lib.Chromosomes
       
         public IChromosome<T> SetRandomValues()
         {
-            Genes = Genes
-                .Select(g => g.CreateNew(
-                    _problem.AllowedValues
-                        .ElementAt(_random.Next(0, _problem.ChromosomeLength))))
-                .ToList();
-
+            for (int i = 0; i < _problem.ChromosomeLength; i++)
+            {
+                Genes[i] = new Gene (_problem.AllowedValues
+                    .ElementAt(RandomizerProvider.Current.GetInt(0, _problem.AllowedValues.Length)));
+            } 
+            
             return this;
+        }
+
+        public void ReplaceGenes(int startIndex, Gene[] genes)
+        {
+            if (genes == null) throw new ArgumentNullException(nameof(genes));
+            var length = genes.Length - startIndex;
+            Array.Copy(genes, startIndex, this.Genes, startIndex, length);           
+        }
+
+        public void ReplaceGene(int index, Gene gene)
+        {
+            this.Genes[index] = gene ?? throw new ArgumentNullException(nameof(gene));
         }
 
         public IChromosome<T> CreateNew(IProblem<T> problem)
         {
+            if (problem == null) throw new ArgumentNullException(nameof(problem));
             return new Chromosome<T>(problem);
         }
 
         public IChromosome<T> Clone()
         {
-            //TODO Need Genes Copy
-            return new Chromosome<T>(this._problem);
+            CopyGenes(this.Genes, out var newGenes);
+            return new Chromosome<T>(this._problem){Genes = newGenes};
+        }
+
+        private void CopyGenes(Gene[] fromGenes, out Gene[] toGenes)
+        {
+            if (fromGenes == null) throw new ArgumentNullException(nameof(fromGenes));
+            
+            toGenes = new Gene[fromGenes.Length];
+            Array.Copy(fromGenes, toGenes, fromGenes.Length);
+        }
+
+        public override string ToString()
+        {
+            return string.Join(" ", Genes.Select(g => g.Value)) + $" Fitness: {Fitness}";
         }
     }
 
--- GeneticAlghorithm.Lib/Chromosomes/IChromosome.cs
+++ GeneticAlghorithm.Lib/Chromosomes/IChromosome.cs
@@ -7,9 +7,11 @@ namespace GeneticAlghorithm.Lib.Chromosomes
     public interface IChromosome<T>
     {
         double Fitness { get; }
-        IList<Gene<T>> Genes { get; }
+        Gene[] Genes { get; }
         IChromosome<T> Clone();
         IChromosome<T> CreateNew(IProblem<T> problem);
         IChromosome<T> SetRandomValues();
+        void ReplaceGenes(int startIndex, Gene[] genes);
+        void ReplaceGene(int index, Gene gene);
     }
 }
