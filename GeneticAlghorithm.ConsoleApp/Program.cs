f94d7597ebde3bd34cdb71deb7552d96217b7542
Dariusz Dąbrowski <dariusz.dbr@gmail.com>
2017-11-25 12:44:40 +0100

    changed the condition of predicate implementation for IGAEngine.Run method

--- GeneticAlghorithm.ConsoleApp/Program.cs
+++ GeneticAlghorithm.ConsoleApp/Program.cs
@@ -23,39 +23,35 @@ namespace GeneticAlghorithm.ConsoleApp
 
         public static void Run()
         {
-            // Settings
-            int populationSize = 6;
-            double crossoverChance = 0.78;
-            double mutationChance = 0.01;
+	        int counter = 0;
 
-            IProblem<int> problem = new MaximumOfQuadraticFuncionInRangeProblem();
+			// Genetic Algorithm Settings 
+			int populationSize = 10;
+            double crossoverChance = 0.78;
+            double mutationChance = 0.05;
+	        int maxNumberOfNotChangedBestChromosome = 50;
+
+			IProblem<int> problem = new MaximumOfQuadraticFuncionInRangeProblem();
             ISelection<int> selection = new RouletteWheelSelection<int>();
             ICrossover<int> crossover = new OnePointCrossover<int>();
             IMutation<int> mutation = new FlipGeneMutation<int>();
             IGAEngine<int> algorithm = new GeneticAlgorithmEngine<int>(populationSize, crossoverChance, mutationChance, problem, mutation, crossover, selection);
 
-            
-            var bestChromosome = algorithm.BestChromosome;  // null         
-            int maxNumberOfNotChangedBestChromosome = 300;
-            int counter = 0;
-            algorithm.Run(() =>
-            {
-                counter++;
-                
-                if (bestChromosome == null || bestChromosome.Fitness < algorithm.BestChromosome.Fitness)
-                {
-                    bestChromosome = algorithm.BestChromosome;
-                    counter = 0;
-                    Console.WriteLine("new best " + bestChromosome);
-                }
+			var bestChromosome = algorithm.BestChromosome;  // null 
+			algorithm.Run(() =>
+			{	
+				counter++;
+				if (bestChromosome == null || !bestChromosome.Fitness.Equals(algorithm.BestChromosome.Fitness))
+				{
+					bestChromosome = algorithm.BestChromosome;
+					counter = 0;
+				}
+				
+				return counter < maxNumberOfNotChangedBestChromosome;
 
-                return counter < maxNumberOfNotChangedBestChromosome;
+			}, writeDetails: true);
 
-            }, writeDetails: false);
-
-            Console.WriteLine(bestChromosome.ToString());
-
-
+			Console.ReadKey();
         }
     }
 }
