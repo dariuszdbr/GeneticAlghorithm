c869cf46e03e0228600b6f35e59130eebffb89b0
Dariusz Dąbrowski <dariusz.dbr@gmail.com>
2017-11-25 13:42:00 +0100

    changed type o X to double

--- GeneticAlghorithm.Lib/Problems/MaximumOfQuadraticFuncionInRangeProblem.cs
+++ GeneticAlghorithm.Lib/Problems/MaximumOfQuadraticFuncionInRangeProblem.cs
@@ -10,16 +10,17 @@ namespace GeneticAlghorithm.Lib.Problems
         public int[] AllowedValues { get; set; } = {0, 1};
         public double FitnessFunc(IChromosome<int> chromosome)
         {
-            int X = Convert.ToInt32(
+            double X = Convert.ToInt32(
                 string.Join("", chromosome.Genes.Select(g => g.Value.ToString())), 2);
 
-            if (X == 0) return 1 / double.MaxValue;
+			// need only if function cannot be calculated ( ln(0) -> undefined ) probability of X = 0 grows when the maximum value of the function is near 0 ( eg,: a < 0; b > 0; c > 0)
+			if (X.Equals(0)) X = 1 / double.MaxValue;
 
             const double A = 2.0;
             const double B = 3.0;
             const double C = 4.0;
 
-            //f(x) = ax^2 + b * lnx - c * sqrt(x^2 + 1);
+            // f(x) = ax^2 + b * lnx - c * sqrt(x^2 + 1);
             return Math.Pow(X, 2.0) * A + B * Math.Log(X) + C * Math.Sqrt(Math.Pow(X, 2.0) + 1);
 			
 			// f(x) = ax^2 + b*x + c
